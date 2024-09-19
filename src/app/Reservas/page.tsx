12bc47348a5e73c1ae2a2bc42d40d8eb2da40d05
rebeca018 <rebecasnascimento018@gmail.com>
2024-09-19 15:27:27 -0400

    Commit aula 19/09

--- src/app/Reservas/page.tsx
+++ src/app/Reservas/page.tsx
@@ -3,8 +3,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import { useState } from "react";
+import { useRouter } from "next/router";
+import Link from "next/link";
 
 export default function Home() {
+  const router = useRouter();
   const [user, setUser] = useState(true);
   if(user){
     return (
@@ -12,7 +15,7 @@ export default function Home() {
         
         <h1>Cliente</h1>
         <button onClick={() => setUser(false)}>User</button>
-        <button> </button>
+        
       </div>
     );
   } else{
