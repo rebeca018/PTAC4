7fb84cb5f6f0d0fbb17e1f6f45de9200c9fddae1
Rebeca <rebecasnascimento018@gmail.com>
2024-09-21 11:56:30 -0400

    Commit

--- src/app/Reservas/page.tsx
+++ src/app/Reservas/page.tsx
@@ -1,32 +1,8 @@
-'use client'
-
-import Image from "next/image";
-import styles from "./page.module.css";
-import { useState } from "react";
-import { useRouter } from "next/router";
-import Link from "next/link";
-
-export default function Home() {
-  const router = useRouter();
-  const [user, setUser] = useState(true);
-  if(user){
-    return (
-      <div className={styles.page}>
-        
-        <h1>Cliente</h1>
-        <button onClick={() => setUser(false)}>User</button>
-        
-      </div>
-    );
-  } else{
-    return (
-      <div className={styles.page}>
-
-        <h1>Administrador</h1>
-        <button onClick={() => setUser(true)}>User</button>
-      </div>
-    );
-  }
-
+export default function Reservas() {
+  return(
+    <div>
+      <h1>Reserva</h1>
+    </div>
+  )
 
 }
